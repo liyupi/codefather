bb743d0242ded657061161c8fb244cf34bed584a
liyupi <592789970@qq.com>
2023-12-06 22:35:38 +0800

    fix 修复字体大小 bug

--- .vuepress/sidebar.ts
+++ .vuepress/sidebar.ts
@@ -12,12 +12,10 @@ export default {
     "/学习路线/": roadmapSideBar,
     "/项目实战/": projectSideBar,
     "/编程导航/": codeNavSideBar,
-    "/算法通关/": AlgorithmClearanceSideBar,
     "/产品服务/": productSideBar,
     "/知识碎片/": knowledgeSideBar,
     "/自学之路/": selfStudySideBar,
     "/编程分享/": programmingShareSideBar,
-
     "/关于我们/": ["", "个人经历"],
     // 降级，默认根据文章标题渲染侧边栏
     "/": "auto",
